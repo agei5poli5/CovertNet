b0438a2899fb4035f7069a9c0601257d45b86ad4
Computer Science <cs@RKC100-05bardedu.local>
2019-11-18 17:16:40 -0500

    Commity

--- RequestHandler.cpp
+++ RequestHandler.cpp
@@ -17,10 +17,10 @@
   RequestHandler::RequestHandler(const char* serverip, unsigned short int serverport){
     opt = 1;
     addrlen = sizeof(proxyaddr);
-    
+
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(serverport);
-    inet_aton(serverip, &serveraddr.sin_addr);
+    serveraddr.sin_addr.s_addr = inet_addr(serverip);
 
     
     proxyaddr.sin_family = AF_INET;
@@ -54,8 +54,26 @@
       valread = read(client, buffer, 1024);
       std::vector<std::string> parsed = parseRequest(buffer);
       char* parsey = "hiya";
-      send(client, parsey, strlen(parsey), 0);
-      std::vector<std::string> newrequest = createRequest(parsed);
+      const char* post = "POST /helloservlet/sayhello HTTP/1.1 \nHost: 10.10.53.67:8080 \nUser-Agent:Mozilla/5.0 \nAccept: text/xml,text/html,text/plain,/image/jpeg \nAccept-Language: en-us,en \nAccept-Encoding: gzip, deflate \nAccept-Charset:ISO-8859-1.utf-8 \nKeep-Alive:300 \nConnection:keep-alive";
+      if ((serversock = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      if (setsockopt(serversock, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    }
+      if (bind(serversock, (struct sockaddr *)&serveraddr,  
+                                 sizeof(serveraddr))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    }
+      send(serversock, post, strlen(post), 0);  
+      
+      //std::vector<std::string> newrequest = createRequest(parsed);
       }
 }
    std::vector<std::string> RequestHandler::parseRequest(char request[]){
@@ -70,13 +88,13 @@
     std::vector<std::string> RequestHandler::createRequest(std::vector<std::string> parsed) 
     {
        std::string url = "foo.php";
-       std::cout << parsed[0];
+       //std::cout << parsed[0];
        std::vector<std::string> request;
        auto it = request.begin();
        request.insert(it, "POST" + url + "HTTP/1.1");
        it = request.begin();
        request.insert(it++, "Host: " + url);
-       std::cout << request.front() + "\n";
+       //std::cout << request.front() + "\n";
        return request;
       }
       
